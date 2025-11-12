c8aacc6d3ad9dd8c7adf367673bd5a24fcde6f18
huseynalp <huseynalph@gmail.com>
2025-11-12 22:33:34 +0000

    Add concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-str = ("Python is an interpreted, interactive, object-oriented programming "
-       "language that combines remarkable power with very clear syntax")
-str = (str[str.find("object-oriented"):str.find("programming") + 11]
-       + " with Python")
-print(str)  # do not change this line
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"                           str = str[str.find("object-oriented") : str.find("programming") + 11] + " with Python" 
+print(str)  # do not change this line must be like this
