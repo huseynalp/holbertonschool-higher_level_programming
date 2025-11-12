5561139dadd79413525faa412841f7b80343e2b0
huseynalp <huseynalph@gmail.com>
2025-11-12 22:13:36 +0000

    Add concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[53:78] + str[-6:]
+str = str[51:78] + " with Python"
 print(str) # do not change this line
