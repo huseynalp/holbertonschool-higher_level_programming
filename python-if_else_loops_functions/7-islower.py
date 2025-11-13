c818b866041363c9074262c9d74d871b70c53646
huseynalp <huseynalph@gmail.com>
2025-11-13 08:10:47 +0000

    Add lowercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord(a) <= ord(c) <= ord(z)
+    if len(c)!=1:
+	raise ValueError("Input must be a single character")
+
+    char_code=ord(c)
+    return 97<= char_code <= 122
+
