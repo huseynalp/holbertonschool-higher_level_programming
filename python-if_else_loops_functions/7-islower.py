7db37561c07892ef65588b08ba0d2d9c21c7e0ee
huseynalp <huseynalph@gmail.com>
2025-11-13 08:59:22 +0000

    Add lowercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     """Check if a character is lowercase."""
-    if ord(c) >= ord(a) and ord(c) <= ord(z):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
