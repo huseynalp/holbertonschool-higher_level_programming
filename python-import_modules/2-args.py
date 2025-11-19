0e4c03d04019686f94d1a45eac40bdc3f9ea7642
huseynalp <huseynalph@gmail.com>
2025-11-19 15:09:22 +0000

    Add argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-  count = len(argv) - 1
+from sys import argv
 
-  if cpunt == 0:
-    print("0 arguments.")
-  else:
-    if count == 1:
-      print("1 argument:")
+if __name__ == "__main__":
+    count = len(argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
     else:
-      print("{} arguments:".format(count))
-    for i in range(1,len(argv)):
-     print("{}: {}".format(i, argv[i]))
+        if count == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(count))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
