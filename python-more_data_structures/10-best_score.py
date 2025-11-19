51095b84888167474bd8225c2dea44e3f8f694a3
huseynalp <huseynalph@gmail.com>
2025-11-19 20:08:38 +0000

    Add best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    """Return key with biggest integer value."""
+    """Return key with the biggest integer value."""
     if not a_dictionary:
         return None
     best_key = None
-    max_val = float(-inf)  # use -inf as a string
+    max_val = float(-inf)  # negative infinity
     for k, v in a_dictionary.items():
         if v > max_val:
             max_val = v
