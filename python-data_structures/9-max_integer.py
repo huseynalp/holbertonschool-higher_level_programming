4c8cb98fdb91300509973a3c2906c70f2a3258ad
huseynalp <huseynalph@gmail.com>
2025-11-19 18:33:53 +0000

    Add max integer

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Find the biggest integer in a list.
+
+    Returns None if the list is empty.
+    """
+    if not my_list:
+        return None
+
+    max_val = my_list[0]
+    for num in my_list:
+        if num > max_val:
+            max_val = num
+    return max_val
