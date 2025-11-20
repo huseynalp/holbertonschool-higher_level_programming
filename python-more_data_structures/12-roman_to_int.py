a9ed3b18fe12511d043cca463b38876ab8946240
huseynalp <huseynalph@gmail.com>
2025-11-20 08:40:42 +0000

    Add roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,8 +10,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    roman_map = {I: 1, V: 5, X: 10, L: 50,
-                 C: 100, D: 500, M: 1000}
+    roman_map = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
 
     total = 0
     prev = 0
