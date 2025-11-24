443e7ca48fa6155a0ab2351c9668dc05fb761014
huseynalp <huseynalph@gmail.com>
2025-11-24 11:57:23 +0000

    Add list of available attributes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module 0-lookup: returns list of available attributes and methods of an object."""
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)
