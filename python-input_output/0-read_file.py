174c472e47b563e7ff9ceaa1ec35052a9ecd8154
huseynalp <huseynalph@gmail.com>
2025-11-26 20:36:50 +0000

    Add defines a function

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file and prints it."""
+
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its contents to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
