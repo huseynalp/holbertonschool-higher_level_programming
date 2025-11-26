2f4988cd8ebdaad29e348393bf5622103e7359e3
huseynalp <huseynalph@gmail.com>
2025-11-26 20:39:05 +0000

    README.md for read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python3
-"""Defines a function that reads a text file and prints it."""
-
-
-def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its contents to stdout."""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+README.md for read file
