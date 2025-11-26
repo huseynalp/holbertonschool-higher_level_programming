01f2300195ee06e4182a5262486802b2ac99a102
huseynalp <huseynalph@gmail.com>
2025-11-26 22:05:26 +0000

    Add module defining a student class with json serialize

---
+++ python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module defining a Student class with JSON serialization support."""
+
+
+class Student:
+    """Represents a student with basic attributes."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes in that list
+        are included. Otherwise, all attributes are included.
+        """
+        if isinstance(attrs, list) and all(
+            isinstance(a, str) for a in attrs
+        ):
+            return {
+                k: v
+                for k, v in self.__dict__.items()
+                if k in attrs
+            }
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance.
+
+        Args:
+            json (dict): Dictionary containing attribute names and values
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
