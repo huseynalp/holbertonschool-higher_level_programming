d0b3439626a5822802063e36003280e0d9d1e621
huseynalp <huseynalph@gmail.com>
2025-11-26 20:54:46 +0000

    Add function that appends text

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Function that appends text to the end of a file."""
-
+"""Function that appends text to a UTF-8 file."""
 
 def append_write(filename="", text=""):
-    """Appends a string to a UTF8 text file and returns number of characters."""
+    """Append a string at the end of a UTF-8 text file."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
