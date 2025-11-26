cd4467281efeddf8b616daae8928bb23671fd2f0
huseynalp <huseynalph@gmail.com>
2025-11-26 21:40:58 +0000

    Add class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Function that returns the dictionary description of an object for JSON serialization."""
+"""Function that returns the dictionary description of an object
+for JSON serialization.
+"""
 
 
 def class_to_json(obj):
