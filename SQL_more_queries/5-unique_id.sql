e20051fdeef4f3e25a355c788825fe4a76e28e9c
huseynalp <huseynalph@gmail.com>
2025-11-29 14:34:42 +0000

    Add new table unique_id

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- 5-unique_id.sql
+-- Creates the table unique_id with id INT default 1 and unique, and name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
