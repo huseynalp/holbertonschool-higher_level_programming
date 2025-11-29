73fc47e38e0de30f10c22a492c4961043d1866cc
huseynalp <huseynalph@gmail.com>
2025-11-29 14:53:56 +0000

    Add list all cities with their corresponding state names

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- 9-cities_by_state_join.sql
+-- List all cities with their corresponding state names
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
