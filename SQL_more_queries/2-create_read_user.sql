b41d05bf037602570d0f5fd745a5a7897d129857
huseynalp <huseynalph@gmail.com>
2025-11-29 14:03:17 +0000

    Add secondd database and user with select privilege

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2-create_read_user.sql
+-- Creates the database hbtn_0d_2 and the user user_0d_2 with SELECT privilege
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
