2a24ccd5dc082f53dab092666ac02b6c7d3a8830
huseynalp <huseynalph@gmail.com>
2025-11-29 13:01:22 +0000

    Add new table 2

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- 9-full.creation sql
+-- Creation of second_table with multiple rows
+
+CREATE TABLE second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert multiple rows into table
+
+INSERT INTO sceond_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
