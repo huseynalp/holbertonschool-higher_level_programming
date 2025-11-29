dfc5a7980f7cdaaca2df7960489265123295c31c
huseynalp <huseynalph@gmail.com>
2025-11-29 14:32:12 +0000

    Add new table if_not_null

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- 4-never_empty.sql
+-- Creates the table id_not_null with id INT default 1 and name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
