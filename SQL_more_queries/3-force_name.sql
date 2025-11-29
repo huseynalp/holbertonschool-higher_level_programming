1d7306c1a3cab5768280c17024426cfb0469adc5
huseynalp <huseynalph@gmail.com>
2025-11-29 14:30:00 +0000

    Add new table force_name

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- 3-force_name.sql
+-- Creates the table force_name with id INT and name VARCHAR(256) NOT NULL
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
