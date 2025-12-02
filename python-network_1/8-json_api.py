1cfce1688b86a9c003d18bf43c433eeb3593d77d
huseynalp <huseynalph@gmail.com>
2025-12-02 05:31:46 +0000

    Add send a post request to search_user with a letter as parameter and displays the result depending on the json response

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to search_user with a letter as parameter.
+Displays the result depending on the JSON response.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    try:
+        response = requests.post(url, data={"q": q})
+        data = response.json()  # may raise ValueError
+
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
